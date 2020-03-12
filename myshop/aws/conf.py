12732044cd23cca823169d14842aa029fff5c1f9
Joseph Nganga <kajoengasa@gmail.com>
2020-03-12 18:02:30 +0300

    Update conf.py

--- myshop/aws/conf.py
+++ myshop/aws/conf.py
@@ -1,17 +1,16 @@
 import datetime
 import os 
 import boto3
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'a2dstorebucket'
+AWS_ACCESS_KEY_ID = 'AKIAIH2QAX4L37SZTW2A'
+AWS_SECRET_ACCESS_KEY = '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'
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
 
 DEFAULT_FILE_STORAGE = 'myshop.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'myshop.aws.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'kscheung1228myshop'
-S3DIRECT_REGION = 'us-west-2'
+S3DIRECT_REGION = 'us-east-2'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
